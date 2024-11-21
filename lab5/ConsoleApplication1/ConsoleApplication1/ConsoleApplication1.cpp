fc22b18333fc0b7f7fa4e61cfc6bb7ed56a95caa
Artem Lotikov <artemlotikov@gmail.com>
2024-11-21 17:35:36 +0200

    lab

---
+++ lab5/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+ofstream fout("resultsForY.txt");
+
+// Функція перевірки вхідних даних
+void checkData(int n, double x, double b, double h)
+{
+    if (n < 3 || x > b || h <= 0)
+    {
+        throw "incorrect";
+    }
+}
+
+// Функція для отримання даних від користувача
+void getData(int& n, double& a, double& b, double& h, double& yesNO) {
+    cout << "Enter n: "; // Введіть n
+    cin >> n;
+    cout << "Enter a: "; // Введіть a
+    cin >> a;
+    cout << "Enter b: "; // Введіть b
+    cin >> b;
+    cout << "Enter h: "; // Введіть h
+    cin >> h;
+    cout << "Enter 0 to save answers to a file: "; // Введіть 0, щоб записати відповіді у файл
+    cin >> yesNO;
+}
+
+// Функція для підрахунку значень функції та їх виведення
+double y(int n, double x)
+{
+    double y = 0;
+    if (x > 0)
+    {
+        for (int i = 0; i <= n - 1; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                y += (i + 1) / (j + x);
+            }
+        }
+    }
+    else
+    {
+        for (int i = 2; i <= n - 1; i++)
+        {
+            y += (x - 1) / i;
+        }
+    }
+    return y;
+}
+
+// Функція для друку результатів
+void Print(double a, double b, double h, double yesNO, int n)
+{
+    double total = 0;
+    for (double x = a; x <= b; x += h)
+    {
+        total += y(n, x);
+        cout << " (x=" << x << ")" << ", y = " << y(n, x) << endl;
+        if (yesNO == 0)
+        {
+            fout << " (x=" << x << ")" << ", y = " << y(n, x) << endl;
+        }
+    }
+    cout << "\nTotal sum = " << total; // новий елемент програми
+}
+
+int main() {
+    setlocale(LC_ALL, "UKRAINIAN"); // встановлення української локалізації
+    int n;
+    double a, b, h, yesNO;
+    getData(n, a, b, h, yesNO);
+    try // перевірка
+    {
+        checkData(n, a, b, h);
+    }
+    catch (const char* exit) // відома помилка
+    {
+        cout << exit;
+        return -1;
+    }
+    catch (...) // невідома помилка 
+    {
+        cout << "Enter different values "; // Введіть інші значення
+        return -2;
+    }
+    Print(a, b, h, yesNO, n);
+    return 0;
+}
---
+++ lab5/ConsoleApplication1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,71 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "D:\c++ lab\lab5\ConsoleApplication1\ConsoleApplication1\ConsoleApplication1.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(CalAndPrint)
+	{
+	public:
+
+		TEST_METHOD(calculate1)
+		{
+			int n = 3;
+			double x = -3;
+			double Expected = -2;
+			double Actual = y(n, x);
+			Assert::AreEqual(Expected, Actual);
+		}
+	public:
+
+		TEST_METHOD(calculate2)
+		{
+			int n = 4; double x = -11;
+			double Expected = -10;
+			double Actual = y(n, x);
+			Assert::AreEqual(Expected, Actual);
+		}
+	public:
+
+		TEST_METHOD(calculate3)
+		{
+			int n = 5;
+			double x = 0;
+			double Expected = -1000;
+			double Actual = y(n, x);
+			Assert::AreEqual(Expected, Actual);
+		}
+	};
+	TEST_CLASS(cheD)
+	{
+	public:
+		TEST_METHOD(Data1)
+		{
+			int n = 3; double x = -1, b = 1, h = 0.1;
+			try
+			{
+				checkData(n, x, b, h);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+	public:
+		TEST_METHOD(Data2)
+		{
+			int n = -1; double x = 0.4, b = 11, h = 0.7;
+			try
+			{
+				checkData(n, x, b, h);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+	};
+}
